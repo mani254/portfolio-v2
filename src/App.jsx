3c1ba903eebf96bef8749fb54bb3e22c391d5cbf
mani254 <manifreelancer25@gmail.com>
2024-09-17 23:34:20 +0530

    initial commit after creating project vite and tailwind css along with the lenis smooth scroll implimentation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SmoothScroll from "./components/smoothScroll";
+
+import "./App.css";
+
+function App() {
+	return (
+		<SmoothScroll>
+			<div className="text-2xl font-bold text-center"> Hello World!</div>
+		</SmoothScroll>
+	);
+}
+
+export default App;
